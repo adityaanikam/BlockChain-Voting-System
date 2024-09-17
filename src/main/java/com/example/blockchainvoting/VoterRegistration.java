649c160ed553aab83ec737b6bfba0a70e386b292
Aditya Nikam <118399608+adityaanikam@users.noreply.github.com>
2024-09-17 21:42:19 +0530

    Update VoterRegistration.java

--- src/main/java/com/example/blockchainvoting/VoterRegistration.java
+++ src/main/java/com/example/blockchainvoting/VoterRegistration.java
@@ -2,18 +2,12 @@ package com.example.blockchainvoting;
 
 import java.util.HashSet;
 
-<<<<<<< HEAD
-import java.util.HashSet;
+
 
 public class VoterRegistration {
     private HashSet<String> registeredVoters = new HashSet<>();
 
     // Register a voter by their voter ID
-=======
-public class VoterRegistration {
-    private HashSet<String> registeredVoters = new HashSet<>();
-
->>>>>>> 6539ad1557721f499e57d578f03a82981f6cf265
     public boolean registerVoter(String voterId) {
         if (registeredVoters.contains(voterId)) {
             System.out.println("Voter already registered.");
@@ -24,10 +18,7 @@ public class VoterRegistration {
         return true;
     }
 
-<<<<<<< HEAD
     // Check if a voter is registered
-=======
->>>>>>> 6539ad1557721f499e57d578f03a82981f6cf265
     public boolean isVoterRegistered(String voterId) {
         return registeredVoters.contains(voterId);
     }
